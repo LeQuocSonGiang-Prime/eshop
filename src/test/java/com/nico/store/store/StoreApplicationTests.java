9cdc5bd21851284680594b9aaf64b4e05b9ea99a
LeQuocSonGiang <gianglqs07@gmail.com>
2023-07-28 00:19:03 +0700

    make it better

--- src/test/java/com/nico/store/store/StoreApplicationTests.java
+++ src/test/java/com/nico/store/store/StoreApplicationTests.java
@@ -2,11 +2,13 @@ package com.nico.store.store;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StoreApplicationTests {
 
 	@Test
