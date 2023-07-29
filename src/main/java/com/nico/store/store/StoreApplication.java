5496bf87bec1221fb500bba7a4bb6c75e4c18de4
LeQuocSonGiang <gianglqs07@gmail.com>
2023-07-29 23:48:37 +0700

    update

--- src/main/java/com/nico/store/store/StoreApplication.java
+++ src/main/java/com/nico/store/store/StoreApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Collections;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.nico.store.store.*"})
 
@@ -11,7 +14,14 @@ import org.springframework.context.annotation.ComponentScan;
 public class StoreApplication  {	
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		String port = System.getenv("PORT");
+		if (port == null) {
+			port = "8080"; // Nếu không tìm thấy biến PORT, sử dụng cổng mặc định 8080
+		}
+		SpringApplication app = new SpringApplication(StoreApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+		//SpringApplication.run(StoreApplication.class, args);
 	}
 
 }
